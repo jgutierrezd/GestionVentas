a6afa9368b44c49f70309786382389e31b13d32a
Jorge Ernesto Gutierrez Duenas <jorgegutierrez@bcp.com.pe>
2023-07-14 14:38:24 -0500

    seguridad API - version inicial

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,18 +1,84 @@
 using ApiGestionVentas.Data;
-using ApiGestionVentas.Models;
-using Microsoft.AspNetCore.Mvc;
+using ApiGestionVentas.ViewModel;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
+
+// Add services DB to container.
 builder.Services.AddDbContext<GestionVentasDbContext>();
 
+// Add services Authentication(JWT) to container.
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = true
+    };
+});
+
+// Add services Authorization to container.
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Map access token
+app.MapPost("/token",  async (User user) =>
+{
+    if (user.UserName == "Admin" && user.Password == "123456")
+    {
+        var issuer = builder.Configuration["Jwt:Issuer"];
+        var audience = builder.Configuration["Jwt:Audience"];
+        var key = Encoding.ASCII.GetBytes
+        (builder.Configuration["Jwt:Key"]);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim("Id", Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Email, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti,
+                Guid.NewGuid().ToString())
+             }),
+            Expires = DateTime.UtcNow.AddMinutes(5),
+            Issuer = issuer,
+            Audience = audience,
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
+        };
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var jwtToken = tokenHandler.WriteToken(token);
+        var stringToken = tokenHandler.WriteToken(token);
+
+        return Results.Ok(stringToken);
+    }
+    return Results.Unauthorized();
+});
+
 
 app.MapGet("/", () => "Hello World!");
 
 app.MapGet("/products", async (GestionVentasDbContext db) =>
  await db.Producto.ToListAsync()
-);
+).RequireAuthorization();
 
 app.Run();
